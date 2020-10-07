f358dd2a139dbba7932acdff135d9376f004da1c
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-07 13:23:37 +0200

    Update videos.js

--- components/videos.js
+++ components/videos.js
@@ -5,11 +5,27 @@ function Videos() {
   useEffect(() => {
     database.ref('posts').orderByChild('timestamp').limitToLast(videosLimit).on('value', data => setVideosData(data.val()) );
     
-    $('#videos-container').scroll(function() {
-      if($('#videos-container').scrollTop() + $('#videos-container').height() == $(document).height()) {
-        alert("bottom!");
-      }
-    });
+      var windowHeight = $(window).height(),
+    gridTop = windowHeight * .1,
+    gridBottom = windowHeight + $('ul li:last').height();
+
+
+  // On each scroll event on window
+  $(window).on('scroll', function() {
+
+    // Interested element caching
+    var $lastElement = $('#videos-container .video-container:last');
+    // Get elemets top
+    var thisTop = $lastElement.offset().top - $(window).scrollTop();
+
+
+    // Check if the element is in the current viewport
+    if (thisTop > gridTop && (thisTop + $lastElement.height()) < gridBottom) {
+      console.log('Yay! In sight');
+    } else {
+      console.log('Still not available');
+    }
+  });
   }, [videosLimit])
 
   return (
@@ -22,4 +38,13 @@ function Videos() {
       }
     </div>
   );
+}
+
+function isScrolledIntoView(elem)
+{
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
