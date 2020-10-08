4a3ad8ba177343ea91651bfc4fb51ef0bb33cf95
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-08 20:01:13 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,3 @@ window.oncontextmenu = function(event) {
      event.stopPropagation();
      return false;
 };
-
-var keyDownHandler = function(evt) {
-  if (evt.key === 'AudioVolumeDown') {
-    alert('volume down');
-  } else if (evt.key === 'AudioVolumeUp') {
-    alert('volume uo');
-  }
-  evt.preventDefault(); // to stop system app from processing keydown event
-};
-
-window.addEventListener('keydown', keyDownHandler);
