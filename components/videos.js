82e77017a841aa1bfc1d11a06dd6cbc78ca5fe57
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-09 10:38:03 +0200

    Update videos.js

--- components/videos.js
+++ components/videos.js
@@ -30,12 +30,3 @@ function Videos() {
     </div>
   );
 }
-
-function isScrolledIntoView(elem)
-{
-    var docViewTop = $(window).scrollTop();
-    var docViewBottom = docViewTop + $(window).height();
-    var elemTop = $(elem).offset().top;
-    var elemBottom = elemTop + $(elem).height();
-    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
