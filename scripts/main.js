6bbb08d3ac970127e2648a1079811e9e1533d7ef
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-09 10:39:16 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,14 @@
 window.oncontextmenu = function(event) {
-     event.preventDefault();
-     event.stopPropagation();
-     return false;
+   event.preventDefault();
+   event.stopPropagation();
+   return false;
 };
+
+function isScrolledIntoView(elem) {
+  var docViewTop = $(window).scrollTop();
+  var docViewBottom = docViewTop + $(window).height();
+  var elemTop = $(elem).offset().top;
+  var elemBottom = elemTop + $(elem).height();
+  return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
