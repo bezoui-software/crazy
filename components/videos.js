cc2fc3f89896f15dca9c5bf8a31a5024f38ec90c
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-10-09 10:43:45 +0200

    Update videos.js

--- components/videos.js
+++ components/videos.js
@@ -5,13 +5,6 @@ function Videos() {
   
   const increaseVideosLimit = () => setVideosLimit(videosLimit + videosInitialLimit);
   
-  const renderVideos = () => {
-    Object.keys(videosData).reverse().map(key => {
-      const videoData = videosData[key];
-      return <Video videoData={ videoData } />;
-    })
-  }
-  
   useEffect(() => {
     $('#videos-container').on('scroll', function() {
       if (isScrolledIntoView($(`#videos-container .video-container:nth-child( ${videosLimit - 1} )`))) increaseVideosLimit();
@@ -24,7 +17,12 @@ function Videos() {
 
   return (
     <div id='videos-container'>
-        <renderVideos />
+      {
+        Object.keys(videosData).reverse().map(key => {
+          const videoData = videosData[key];
+          return <Video videoData={ videoData } />;
+        })
+      }
     </div>
   );
 }
