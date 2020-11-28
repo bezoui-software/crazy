cb8f393f2aa6e56e160396d24acc346f0230f37a
bezoui-software <64211294+bezoui-software@users.noreply.github.com>
2020-11-28 11:31:57 +0100

    Update videos.js

--- components/videos.js
+++ components/videos.js
@@ -1,18 +1,42 @@
+const { useReducer } = React;
+
+function videosLimitReducer(state, action) {
+  switch (action.type) {
+    case 'increase':
+      return { limit: state.limit + state.increaseAmount, increaseAmount: state.increaseAmount, decreaseAmount: state.decreaseAmount };
+      break;
+    case 'decrease':
+      return { limit: state.limit - state.decreaseAmount, increaseAmount: state.increaseAmount, decreaseAmount: state.decreaseAmount };
+      break;
+    case 'reset':
+      return {limit: 5, increaseAmount: state.increaseAmount, decreaseAmount: state.decreaseAmount }; 
+      break;
+  }
+}
+
 function Videos() {
   const [videosData, setVideosData] = useState({});
-  const [videosInitialLimit, setVideosInitialLimit] = useState(5);
-  const [videosLimit, setVideosLimit] = useState(videosInitialLimit);
+  const [videosInitialLimit, setVideosInitialLimit] = useState(2);
+  const [videosLimit, setVideosLimitAction] = useReducer(videosLimitReducer, { limit: 5, increaseAmount: 5, decreaseAmount: 5});
   
-  const increaseVideosLimit = () => setVideosLimit(videosLimit + videosInitialLimit);
-  
+  const increaseVideosLimit = () => setVideosLimitAction({ type: 'increase' });
+  const decreaseVideosLimit = () => setVideosLimitAction({ type: 'decrease' });
+  const resetVideosLimit = () => setVideosLimitAction({ type: 'reset' });
+  const fetchPosts = () => {
+    database.ref('posts').orderByChild('timestamp').limitToLast(videosLimit.limit).on('value', data => setVideosData(data.val()) );
+  }
+
   useEffect(() => {
-    $('#videos-container').on('scroll', function() {
-      if (isScrolledIntoView($(`#videos-container .video-container:nth-child( ${videosLimit - 1} )`))) increaseVideosLimit();
+    $('#videos-container').off('scroll');
+    $('#videos-container').on('scroll', () => {
+      if (isScrolledIntoView($(`#videos-container .video-container:nth-child( ${videosLimit.limit} )`))) increaseVideosLimit();
+      if (isScrolledIntoView($(`#videos-container .video-container:nth-child( ${ videosLimit.limit - videosLimit.decreaseAmount - 1 } )`))) decreaseVideosLimit();
+      if (isScrolledIntoView($(`#videos-container .video-container:first`))) resetVideosLimit();
     });
-  }, [])
+  }, [videosLimit])
   
   useEffect(() => {
-    database.ref('posts').orderByChild('timestamp').limitToLast(videosLimit).on('value', data => setVideosData(data.val()) );
+    fetchPosts();
   }, [videosLimit])
 
   return (
